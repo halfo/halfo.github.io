2cec6e6da2a1ad8eaa810ee38baf0b90e5f9a681
Hasib Billah <billah.hasib@yahoo.com>
2015-01-13 22:47:31 +0600

    Solved jQuery Issue

--- public/js/main.js
+++ public/js/main.js
@@ -17,47 +17,50 @@
 })(document);
 
 
+if (typeof jQuery !== 'undefined') {
 // Wiki Page's Search Bar (Fuzzy String Searching)
 
-$(function(){
-    $('input[type="text"]').keyup(function(e){
-      if (e.keyCode == 27) $(this).val("");
-      var searchText = $(this).val();
+  $(function(){
+      $('input[type="text"]').keyup(function(e){
+        if (e.keyCode == 27) $(this).val("");
+        var searchText = $(this).val();
 
-      $('ul > li > a').each(function(){
-        var currentText = $(this).text(),
-            searchTextLength = searchText.length,
-            j = 0;
+        $('ul > li > a').each(function(){
+          var currentText = $(this).text(),
+              searchTextLength = searchText.length,
+              j = 0;
 
-        for (var i in currentText) {
-          if (searchTextLength == j) break;
-          if (currentText [i].toLowerCase() == searchText [j] ||
-              currentText [i] == searchText [j])
-            ++j;
-        }
+          for (var i in currentText) {
+            if (searchTextLength == j) break;
+            if (currentText [i].toLowerCase() == searchText [j] ||
+                currentText [i] == searchText [j])
+              ++j;
+          }
 
-        $(this).parent().toggle(searchTextLength == j);
+          $(this).parent().toggle(searchTextLength == j);
+        });
       });
-    });
-});
-
-
-// Category Page's Coloring
-
-var tag_color = { programming : "rgb(188, 0, 5)",
-                  data_structure : "rgb(100, 69, 57)",
-                  string : "rgb(56, 123, 148)",
-                  graph : "rgb(125, 188, 178)",
-                  math : "rgb(153, 0, 76)",
-                  misc : "rgb(158, 149, 153)"};
-
-$(function(){
-  $('.archive.tag > a').each(function(){
-    var tag = $(this).attr('href').substring (1),
-        color = tag_color [tag];
-
-
-    $(this).css ('background-color', color);
-    $('.archive.category.' + tag).css('border-left-color', color);
   });
-});
+
+
+  // Category Page's Coloring
+
+  var tag_color = { programming : "rgb(188, 0, 5)",
+                    data_structure : "rgb(100, 69, 57)",
+                    string : "rgb(56, 123, 148)",
+                    graph : "rgb(125, 188, 178)",
+                    math : "rgb(153, 0, 76)",
+                    misc : "rgb(158, 149, 153)"};
+
+  $(function(){
+    $('.archive.tag > a').each(function(){
+      var tag = $(this).attr('href').substring (1),
+          color = tag_color [tag];
+
+
+      $(this).css ('background-color', color);
+      $('.archive.category.' + tag).css('border-left-color', color);
+    });
+  });
+
+}
