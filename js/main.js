5b4303adcd21a3d486892a8431ab4bc8c83c7262
Hasib Billah <hasib.7724@gmail.com>
2022-12-24 21:23:21 +0600

    Improve faq accordion UX.

--- js/main.js
+++ js/main.js
@@ -9,3 +9,10 @@ const animationObserver = new IntersectionObserver((elems) => {
 const classes = ['h1:not(.slogan *)', 'h3:not(.slogan *)', '.dp', '.slogan', '.card', '.slider'];
 const elems = document.querySelectorAll([classes].join(', '));
 elems.forEach(el => animationObserver.observe(el));
+
+const detailsList = [...document.getElementsByTagName('details')];
+detailsList.forEach(details =>
+  details.addEventListener('toggle', _ => details.open && detailsList.forEach(
+    other => other.open = other === details,
+  ))
+);
